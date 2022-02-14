c31df43ade27a17557ac614cb43a9f15400c95c7
chris-ian-jones <chrisjones@Chriss-MacBook-Pro.local>
2022-02-14 17:12:22 +0000

    Setup folder structure for core module - also added bare header and footer components

--- ng-telford-leisure-services/src/app/app.module.ts
+++ ng-telford-leisure-services/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     AppRoutingModule,
   ],
   providers: [],
