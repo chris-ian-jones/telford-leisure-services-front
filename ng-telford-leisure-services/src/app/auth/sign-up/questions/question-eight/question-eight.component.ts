5fe6625c6d02d7a15f5ba1529d6cd782c797a6e5
chris-ian-jones <chrisjones@Chriss-MacBook-Pro.local>
2022-03-17 15:54:34 +0000

    TLS-009: added question eight form with functionality to emit answer to sign up parent component

--- ng-telford-leisure-services/src/app/auth/sign-up/questions/question-eight/question-eight.component.ts
+++ ng-telford-leisure-services/src/app/auth/sign-up/questions/question-eight/question-eight.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-question-eight',
@@ -10,10 +11,59 @@ export class QuestionEightComponent implements OnInit {
   @Input() currentPage!: number;
   @Input() totalPages!: number;
   @Output() answerEightEvent = new EventEmitter<any>();
+  @ViewChild('adtInput', {static: false}) adtInput: ElementRef;
+  @ViewChild('hcoInput', {static: false}) hcoInput: ElementRef;
+  @ViewChild('haeInput', {static: false}) haeInput: ElementRef;
+  @ViewChild('hccInput', {static: false}) hccInput: ElementRef;
+  questionEightForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.initQuestionEightForm();
+  }
+
+  initQuestionEightForm() {
+    this.questionEightForm = this.formBuilder.group({
+      membershipType: ['', [Validators.required]],
+    })
+  }
+
+  selectInput(value:string) {
+    this.questionEightForm.controls['membershipType'].setValue(value)
+
+    switch(value) {
+      case 'TLC Adt Resident 16+ - ADT': {
+        setTimeout(() => this.adtInput.nativeElement.focus());
+        break;
+      }
+      case 'Annual Cash Concession - HCO': {
+        setTimeout(() => this.hcoInput.nativeElement.focus());
+        break;
+      }
+      case 'Aspirations All Inclusive - HAE': {
+        setTimeout(() => this.haeInput.nativeElement.focus());
+        break;
+      }
+      case 'Aspirations No Contract - HCC': {
+        setTimeout(() => this.hccInput.nativeElement.focus());
+        break;
+      }
+      default: {
+        setTimeout(() => this.adtInput.nativeElement.focus());
+        break;
+      } 
+    }
+  }
+
+  onClickContinue() {
+    if (this.questionEightForm.valid) {
+      this.answerEightEvent.emit(this.questionEightForm.value)
+    } else {
+      
+    }
   }
 
 }
