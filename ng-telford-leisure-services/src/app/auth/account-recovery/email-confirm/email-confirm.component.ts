a0bc51bebc77652395989f4d7f9c5ebb9f8074d7
chris-ian-jones <chrisjones@Chriss-MacBook-Pro.local>
2022-03-21 22:13:01 +0000

    TLS-013: WIP adding email address form in email confirm component

--- ng-telford-leisure-services/src/app/auth/account-recovery/email-confirm/email-confirm.component.ts
+++ ng-telford-leisure-services/src/app/auth/account-recovery/email-confirm/email-confirm.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-email-confirm',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailConfirmComponent implements OnInit {
 
-  constructor() { }
+  emailForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.initEmailForm();
+  }
+
+  initEmailForm() {
+    this.emailForm = this.formBuilder.group({
+      email: ['', Validators.required],
+    }, {updateOn: 'submit'})
+  }
+
+  onClickContinue() {
+    
   }
 
 }
