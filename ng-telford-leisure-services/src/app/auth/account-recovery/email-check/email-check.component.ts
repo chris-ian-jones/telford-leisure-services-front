ecbc6ae882b46956a6dc4dee77c6b56e93fbc815
chris-ian-jones <chrisjones@Chriss-MacBook-Pro.local>
2022-03-21 22:01:31 +0000

    TLS-013: added validation with error summary to email check component form

--- ng-telford-leisure-services/src/app/auth/account-recovery/email-check/email-check.component.ts
+++ ng-telford-leisure-services/src/app/auth/account-recovery/email-check/email-check.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,10 +9,12 @@ import { Router } from '@angular/router';
 })
 export class EmailCheckComponent implements OnInit {
 
-  emailCheckForm!: FormGroup;
   @ViewChild('yesInput', {static: false}) yesInput: ElementRef;
   @ViewChild('noInput', {static: false}) noInput: ElementRef;
   @Output() changeComponentEvent = new EventEmitter<any>();
+  emailCheckForm!: FormGroup;
+  @ViewChild('errorSummaryDiv', {static: false}) errorSummaryDiv!: ElementRef;
+  errorSummary: any = [];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -31,7 +33,8 @@ export class EmailCheckComponent implements OnInit {
 
   selectInput(value:string) {
     this.emailCheckForm.controls['emailCheck'].setValue(value)
-
+    this.errorSummary.length = 0;
+    
     switch(value) {
       case 'Yes': {
         setTimeout(() => this.yesInput.nativeElement.focus());
@@ -49,6 +52,7 @@ export class EmailCheckComponent implements OnInit {
   }
 
   onClickContinue() {
+    this.errorSummary.length = 0;
     if (this.emailCheckForm.valid) {
       if (this.emailCheckForm.controls['emailCheck'].value === 'Yes') {
         this.router.navigateByUrl('/sign-in')
@@ -56,8 +60,29 @@ export class EmailCheckComponent implements OnInit {
         this.changeComponentEvent.emit('email-confirm')
       }
     } else {
-      
+      this.getAllFormValidationErrors();
     }
+  }
+
+  getAllFormValidationErrors() {
+    Object.keys(this.emailCheckForm.controls).forEach(control => {
+      const controlErrors: ValidationErrors = this.emailCheckForm.get(control).errors;
+      if (controlErrors != null) {
+        Object.keys(controlErrors).forEach(error => {
+          this.errorSummary.push(
+            {
+              control,
+              error
+            }
+          )
+        });
+        setTimeout(() => this.errorSummaryDiv.nativeElement.focus())
+      }
+    });
+  }
+
+  onClickEmailCheckError() {
+    setTimeout(() => this.yesInput.nativeElement.focus());
   }
 
 }
