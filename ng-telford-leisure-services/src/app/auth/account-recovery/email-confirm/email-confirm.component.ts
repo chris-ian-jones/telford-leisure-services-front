83ace5ca8c23e41f70e49b4db853dd6e19cbb0b4
chris-ian-jones <chrisjones@Chriss-MacBook-Pro.local>
2022-03-22 11:18:44 +0000

    TLS-013: added validation and error summary to email confirm form

--- ng-telford-leisure-services/src/app/auth/account-recovery/email-confirm/email-confirm.component.ts
+++ ng-telford-leisure-services/src/app/auth/account-recovery/email-confirm/email-confirm.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { SignUpService } from '../../sign-up/sign-up.service';
 
 @Component({
   selector: 'app-email-confirm',
@@ -8,10 +9,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class EmailConfirmComponent implements OnInit {
 
+  @ViewChild('errorSummaryDiv', {static: false}) errorSummaryDiv!: ElementRef;
   emailForm!: FormGroup;
+  errorSummary: any = [];
 
   constructor(
     private formBuilder: FormBuilder,
+    private signUpService: SignUpService,
   ) { }
 
   ngOnInit() {
@@ -20,12 +24,35 @@ export class EmailConfirmComponent implements OnInit {
 
   initEmailForm() {
     this.emailForm = this.formBuilder.group({
-      email: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
     }, {updateOn: 'submit'})
   }
 
   onClickContinue() {
-    
+    this.errorSummary.length = 0;
+    this.signUpService.removeHashPathFromCurrentPath();
+    if (this.emailForm.valid) {
+      // todo
+    } else {
+      this.getAllFormValidationErrors();
+    }
+  }
+
+  getAllFormValidationErrors() {
+    Object.keys(this.emailForm.controls).forEach(control => {
+      const controlErrors: ValidationErrors = this.emailForm.get(control).errors;
+      if (controlErrors != null) {
+        Object.keys(controlErrors).forEach(error => {
+          this.errorSummary.push(
+            {
+              control,
+              error
+            }
+          )
+        });
+        setTimeout(() => this.errorSummaryDiv.nativeElement.focus())
+      }
+    });
   }
 
 }
