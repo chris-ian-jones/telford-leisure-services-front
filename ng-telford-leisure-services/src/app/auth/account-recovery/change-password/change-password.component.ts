1f19bc942b0d846d45bfa1a54570e3b44f2408ce
chris-ian-jones <chrisjones@Chriss-MacBook-Pro.local>
2022-03-28 17:03:05 +0100

    TLS-014: added validation with error summary and routing to change password component, now able to change password stored in database

--- ng-telford-leisure-services/src/app/auth/account-recovery/change-password/change-password.component.ts
+++ ng-telford-leisure-services/src/app/auth/account-recovery/change-password/change-password.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AccountRecoveryService } from '../account-recovery.service';
+import { PasswordConfirmValidator } from './../../../core/validators/confirm-password';
 
 @Component({
   selector: 'app-change-password',
@@ -9,14 +11,17 @@ import { AccountRecoveryService } from '../account-recovery.service';
 })
 export class ChangePasswordComponent implements OnInit {
 
+  @ViewChild('errorSummaryDiv', {static: false}) errorSummaryDiv!: ElementRef;
   @Input() memberEmail!: string;
   @Input() confirmationCode!: string;
   @Output() changeComponentEvent = new EventEmitter<any>();
   passwordForm!: FormGroup;
+  errorSummary: any = [];
 
   constructor(
     private formBuilder: FormBuilder,
-    private accountRecoveryService: AccountRecoveryService
+    private accountRecoveryService: AccountRecoveryService,
+    private router: Router,
   ) { }
 
   ngOnInit() {
@@ -25,12 +30,26 @@ export class ChangePasswordComponent implements OnInit {
 
   initPasswordForm() {
     this.passwordForm = this.formBuilder.group({
-      password: ['', Validators.required],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=\D*\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{0,}$/)
+      ]],
       confirmPassword: ['', Validators.required],
     }, {updateOn: 'submit'})
   }
 
+  checkPasswords() {
+    const password = this.passwordForm.controls['password'].value;
+    const confirmPassword = this.passwordForm.controls['confirmPassword'].value;
+
+    if (password !== confirmPassword) {
+      this.passwordForm.controls['confirmPassword'].setErrors({match: true})
+    }
+}
+
   onClickContinue() {
+    this.checkPasswords();
     if (this.passwordForm.valid) {
       const payload = {
         email: this.memberEmail,
@@ -40,9 +59,36 @@ export class ChangePasswordComponent implements OnInit {
       this.accountRecoveryService.changePassword(payload).subscribe((response:any) => {
         this.changeComponentEvent.emit('password-reset') // todo create component
       }, error => {
-        // todo
+        this.passwordForm.controls['password'].setErrors({token: true})
+        this.getAllFormValidationErrors();
       })
+    } else {
+      this.getAllFormValidationErrors();
     }
   }
+
+  getAllFormValidationErrors() {
+    Object.keys(this.passwordForm.controls).forEach(control => {
+      const controlErrors: ValidationErrors = this.passwordForm.get(control).errors;
+      if (controlErrors != null) {
+        Object.keys(controlErrors).forEach(error => {
+          this.errorSummary.push(
+            {
+              control,
+              error
+            }
+          )
+        });
+        setTimeout(() => this.errorSummaryDiv.nativeElement.focus())
+      }
+    });
+  }
+
+  reloadCurrentRoute() {
+    const currentUrl = this.router.url;
+    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+      this.router.navigate([currentUrl], {state: { route: 'email-confirm', path: 'forgot-password' }} );
+  });
+}
 
 }
