fa57b967ec89740a1b51898518c65c5e9eb4e778
chris-ian-jones <chrisjones@Chriss-MacBook-Pro.local>
2022-03-28 18:33:35 +0100

    TLS-015: added button to dashboard to allow member to sign out

--- ng-telford-leisure-services/src/app/dashboard/dashboard.component.ts
+++ ng-telford-leisure-services/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onClickSignOut() {
+    this.authService.signOut();
   }
 
 }
