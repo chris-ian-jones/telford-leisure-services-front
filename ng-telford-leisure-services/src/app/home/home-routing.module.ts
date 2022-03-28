102bbf18739c79c3cd8a4627664c9f8c94082592
chris-ian-jones <chrisjones@Chriss-MacBook-Pro.local>
2022-03-28 18:27:54 +0100

    TLS-015: added placeholder text to dashboard component

--- ng-telford-leisure-services/src/app/home/home-routing.module.ts
+++ ng-telford-leisure-services/src/app/home/home-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthenticatedGuard } from '../core/guards/authenticated.guard';
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    canActivate: [AuthenticatedGuard]
   },
 ]
 
