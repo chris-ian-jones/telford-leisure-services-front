4aaa19f0ff5b8e4f078ab75bcfc9f590fb074878
chris-ian-jones <chrisjones@Chriss-MacBook-Pro.local>
2022-05-23 12:25:31 +0100

    TLS: updated prod api url for herokuapp due to typo

--- ng-telford-leisure-services/src/environments/environment.prod.ts
+++ ng-telford-leisure-services/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  api: 'https://telford-leisure-services-back.herokuapp.com/',
-  
+  api: 'https://telford-leisure-services-back.herokuapp.com',
 };
