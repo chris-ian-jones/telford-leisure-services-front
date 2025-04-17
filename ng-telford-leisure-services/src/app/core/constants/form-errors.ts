cea4829aa373701f68a4c7366b7b082a0462bc2d
chris-ian-jones <chris.ian.jones@outlook.com>
2025-04-17 14:31:40 +0100

    TLS-019: Refactored sign-in component to use shared Error Summary component

--- ng-telford-leisure-services/src/app/core/constants/form-errors.ts
+++ ng-telford-leisure-services/src/app/core/constants/form-errors.ts
@@ -1,6 +1,7 @@
 export interface ErrorSummaryItem {
   message: string;
   focusElementId: string;
+  tabIndex: number;
 }
 
 export interface FormErrorMessages {
@@ -13,21 +14,39 @@ export const ERROR_MESSAGES: FormErrorMessages = {
   memberNumber: {
     required: {
       message: 'Enter your member number',
-      focusElementId: 'memberNumber'
+      focusElementId: 'memberNumber',
+      tabIndex: 2
     },
     unauthorized: {
       message: 'Incorrect member number or password',
-      focusElementId: 'memberNumber'
+      focusElementId: 'memberNumber',
+      tabIndex: 3
     },
     error: {
       message: 'Server error',
-      focusElementId: 'memberNumber'
+      focusElementId: 'memberNumber',
+      tabIndex: 4
     }
   },
   password: {
     required: {
       message: 'Enter your password',
-      focusElementId: 'password'
+      focusElementId: 'password',
+      tabIndex: 5
+    }
+  },
+  firstName: {
+    required: {
+      message: 'Enter your first name(s)',
+      focusElementId: 'firstName',
+      tabIndex: 3
+    }
+  },
+  lastName: {
+    required: {
+      message: 'Enter your last name',
+      focusElementId: 'lastName',
+      tabIndex: 4
     }
   }
 };
