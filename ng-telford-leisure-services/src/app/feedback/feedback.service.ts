171cf11bd268541348a4a66d6af64bc0f95a9601
chris-ian-jones <chris.ian.jones@outlook.com>
2025-04-21 20:17:32 +0100

    TLS-020: refactored feedback component to a signal based approach

--- ng-telford-leisure-services/src/app/feedback/feedback.service.ts
+++ ng-telford-leisure-services/src/app/feedback/feedback.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, httpResource } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
 import { Url } from '../core/constants/urls';
 import { Feedback } from '../core/models/feedback';
 
@@ -11,12 +11,32 @@ const authHeaders = new HttpHeaders({
   providedIn: 'root'
 })
 export class FeedbackService {
-  constructor(private readonly http: HttpClient) {}
+  private readonly feedbackData = signal<Feedback | undefined>(undefined);
 
-  createNewFeedback(feedbackData: Feedback) {
-    return this.http.post(`${Url.FEEDBACK}/new`, feedbackData, {
-      headers: authHeaders,
-      observe: 'response'
-    });
+  // constructor(private readonly http: HttpClient) {}
+
+  // createNewFeedback(feedbackData: Feedback) {
+  //   return this.http.post(`${Url.FEEDBACK}/new`, feedbackData, {
+  //     headers: authHeaders,
+  //     observe: 'response'
+  //   });
+  // }
+  createFeedbackResource = httpResource<Feedback>(() => {
+    const data = this.feedbackData();
+    if (!data) {
+      return undefined;
+    }
+    return {
+      method: 'POST',
+      url: `${Url.FEEDBACK}/new`,
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      }),
+      body: data
+    };
+  });
+
+  setFeedbackData(data: Feedback) {
+    this.feedbackData.set(data);
   }
 }
